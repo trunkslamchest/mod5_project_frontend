e68fb42550883d0038feed0c5cc53da0b59f5be7
trunkslamchest <trunk9slamchest@gmail.com>
2020-03-08 19:39:58 -0400

    updated index buttons to use a single HOC

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useCallback } from 'react'
-import { NavLink } from 'react-router-dom'
 
 import index_logo from './assets/index_logo_blue3.png'
+
+import IndexButton from './UI/indexButton'
 
 import './css/Home.css'
 
 const Home = (props) => {
 
 	const { update_page_data, update_traffic_data } = props
-
-	// componentDidMount(){
-	// 	onPageLoadFunctions()
-	// }
 
 	const onClickLogInFunctions = (event) => {
 		onClickUpdateTrafficFunctions(event)
@@ -41,105 +38,100 @@ const Home = (props) => {
 		})
 	}
 
-	const onPageLoadFunctions = useCallback((temp) => {
+	const onPageLoadFunctions = useCallback(() => {
 		update_page_data({
 			user_id: localStorage.user_id,
 			page_name: "index",
-		}, console.log(temp))
+		})
 	}, [update_page_data])
 
 	useEffect(() => {onPageLoadFunctions()}, [onPageLoadFunctions])
 
-		const quick_play_button =
-			<NavLink
-				exact to="/quick_play"
-				key={"quick_play_link"}
-				name="quick_play_button"
-				interaction="click"
-				className="quick_play_button"
-				onClick={onClickQuickPlayFunctions }
-			>
-				Quick Play
-			</NavLink>
+	const quick_play_button =
+		<IndexButton
+			link="/quick_play"
+			buttonName="quick_play_button"
+			interaction="click"
+			classType="quick_play_button"
+			onClick={ onClickQuickPlayFunctions }
+		>
+			Quick Play
+		</IndexButton>
 
-		const play_by_difficulty =
-			<NavLink
-				exact to="/play_by_difficulty"
-				key={"play_by_difficulty_link"}
-				name="play_by_difficulty_button"
-				interaction="click"
-				className="play_by_difficulty_button"
-				onClick={onClickPlayByDifficultyFunctions }
-			>
-				Play By Difficulty
-			</NavLink>
+	const play_by_difficulty =
+		<IndexButton
+			link="/play_by_difficulty"
+			buttonName="play_by_difficulty_button"
+			interaction="click"
+			classType="play_by_difficulty_button"
+			onClick={ onClickPlayByDifficultyFunctions }
+		>
+			Play By Difficulty
+		</IndexButton>
 
-		const play_by_category =
-			<NavLink
-				exact to="/play_by_category"
-				key={"play_by_category_link"}
-				name="play_by_category_button"
-				interaction="click"
-				className="play_by_category_button"
-				onClick={onClickPlayByCategoryFunctions }
-			>
-				Play By Category
-			</NavLink>
+	const play_by_category =
+		<IndexButton
+			link="/play_by_category"
+			buttonName="play_by_category_button"
+			interaction="click"
+			classType="play_by_category_button"
+			onClick={ onClickPlayByCategoryFunctions }
+		>
+			Play By Category
+		</IndexButton>
 
-		const log_in_button =
-			<NavLink
-				exact to="/log_in"
-				key={"log_in_link"}
-				name="log_in_button"
-				interaction="click"
-				className="log_in_button"
-				onClick={onClickLogInFunctions }
-			>
-				Log In
-			</NavLink>
+	const log_in_button =
+		<IndexButton
+			link="/log_in"
+			buttonName="log_in_button"
+			interaction="click"
+			classType="log_in_button"
+			onClick={ onClickLogInFunctions }
+		>
+			Log In
+		</IndexButton>
 
-		const sign_up_button =
-			<NavLink
-				exact to="/sign_up"
-				key={"sign_up_link"}
-				name="sign_up_button"
-				interaction="click"
-				className="sign_up_button"
-				onClick={onClickSignUpFunctions }
-			>
-				Sign Up
-			</NavLink>
+	const sign_up_button =
+		<IndexButton
+			link="/sign_up"
+			buttonName="sign_up_button"
+			interaction="click"
+			classType="sign_up_button"
+			onClick={ onClickSignUpFunctions }
+		>
+			Sign Up
+		</IndexButton>
 
-		const logged_in_home = <>
-				<div className="logo_container">
-					<img src={index_logo} alt={"logo"}/>
-				</div>
-				<div className="play_buttons_container">
-					<div className="quick_play_container">
-						{ quick_play_button }
-					</div>
-					<div className="other_play_container">
-						{ play_by_difficulty }
-						{ play_by_category }
-					</div>
-				</div>
-		</>
-
-		const logged_out_home = <>
+	const logged_in_home = <>
 			<div className="logo_container">
 				<img src={index_logo} alt={"logo"}/>
 			</div>
-			<div className="log_in_sign_up_container">
-				{ log_in_button }
-				{ sign_up_button }
+			<div className="play_buttons_container">
+				<div className="quick_play_container">
+					{ quick_play_button }
+				</div>
+				<div className="other_play_container">
+					{ play_by_difficulty }
+					{ play_by_category }
+				</div>
 			</div>
-		</>
+	</>
 
-		return(
-			<div className="default_wrapper">
-				{ localStorage.length === 0 ? logged_out_home : logged_in_home }
-			</div>
-		)
+	const logged_out_home = <>
+		<div className="logo_container">
+			<img src={index_logo} alt={"logo"}/>
+		</div>
+		<div className="log_in_sign_up_container">
+			{ log_in_button }
+			{ sign_up_button }
+		</div>
+	</>
+
+	return(
+		<div className="default_wrapper">
+			{ localStorage.length === 0 ? logged_out_home : logged_in_home }
+		</div>
+	)
 
 }
 
---
+++ src/UI/indexButton.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './indexButtons.css'
+
+const indexButton = (props) => {
+	return(
+		<NavLink
+			exact to={props.link}
+			name={props.buttonName}
+			interaction={props.interaction}
+			className={props.classType}
+		>
+			{props.children}
+		</NavLink>
+	)
+}
+
+export default indexButton

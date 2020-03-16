14d00f47d77f43f0950117e25fa2a68ffb624363
Austin Smith <trunk9slamchest@gmail.com>
2020-03-16 19:08:02 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -216,33 +216,6 @@ export default class App extends React.Component {
 		})
 	}
 
-	// update_traffic_data = (res_obj) => {
-	// 	fetch("http://localhost:3001/traffics", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			"Content-Type": "application/json"
-	// 		},
-	// 		body: JSON.stringify({
-	// 			user_id: res_obj.user_id,
-	// 			interaction: res_obj.interaction,
-	// 			element: res_obj.element
-	// 		})
-	// 	})
-	// }
-
-	// update_page_data = (res_obj) => {
-	// 	fetch("http://localhost:3001/pages", {
-	// 		method: "POST",
-	// 		headers: {
-	// 			"Content-Type": "application/json"
-	// 		},
-	// 			body: JSON.stringify({
-	// 			user_id: res_obj.user_id,
-	// 			page_name: res_obj.page_name,
-	// 		})
-	// 	})
-	// }
-
 	render(){
 		return (
 			<>
